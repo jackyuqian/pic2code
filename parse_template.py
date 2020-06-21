be5f855d8c73ceb87721efbf72b4dde3d5277e68
qianqyu <qianqyu@tencent.com>
2020-06-21 17:56:04 +0800

    add parse template

--- parse_template.py
+++ parse_template.py
@@ -20,23 +20,43 @@ def find_longest_substring(lst_in, ele):
             ele_length  = 0
     return rslt
 
-def get_params(ftempl):
+def get_params(img_arr):
+    # find care colume
+    care_col    = np.argmax(np.sum(img_arr, axis = 0))
+    row_start, row_length   = find_longest_substring(img_arr[:, care_col], True)
+    row_win     = int(row_length / 8)
+
+    # find care row 
+    care_row    =np.argmax(np.sum(img_arr, axis = 1))
+    col_start, col_length   = find_longest_substring(img_arr[care_row, :], True)
+    col_win     = int(col_length / 64)
+    
+    return [row_start, col_start, row_win, col_win]
+
+def parse_template(ftempl):
+
     # read template and pre-process
     img_rgb = Image.open(ftempl)
     img_bin = img_rgb.convert('L')
     img_arr = np.array(img_bin) > 128
     
-    # find care colume
-    care_col    = np.argmax(np.sum(img_arr, axis = 0))
-    col_start, col_length   = find_longest_substring(img_arr[:, care_col], True)
-    col_win     = (col_length / 8)
+    # get params
+    row_start, col_start, row_win, col_win  = get_params(img_arr)
 
-    # find care row 
-    care_row    =np.argmax(np.sum(img_arr, axis = 1))
-    row_start, row_length   = find_longest_substring(img_arr[care_row, :], True)
-    row_win     = (row_length / 16)
+    # get chars template
+    chars       = list("`1234567890-=~!@#$%^&*()_+qwertyuiop[]\QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:" + '"zxcvbnm,./ZXCVBNM<>? ')
+    ctempl_dict = {}
+    for i in range(len(chars)):
+        row_start_i = row_start + row_win * 8
+        col_start_i = col_start + col_win * i
+        ctempl_dict[chars[i]]   = img_arr[row_start_i : row_start_i + row_win, col_start_i : col_start_i + col_win]
+
+    img = Image.fromarray(ctempl_dict['_'])
+    img.show()
     
     return [row_start, col_start, row_win, col_win]
+
+
 
 def print_usage():
     print('./gen_params.py -i <template>')
@@ -54,7 +74,7 @@ def main(argv):
             sys.exit()
         elif opt in ("-i"):
             fin = arg
-    row_start, col_start, row_win, col_win  = get_params(fin)
+    row_start, col_start, row_win, col_win  = parse_template(fin)
     print("row start= %d" % row_start)
     print("col start= %d" % col_start)
     print("row win  = %d" % row_win)
--- template/gen.py
+++ template/gen.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 with open("template.txt", "w") as fp:
     txt =  ""
-    txt += 16*chr(9608) + '\n'
+    txt += 64*chr(9608) + '\n'
     for i in range(7):
         txt += chr(9608) + '\n'
-    txt += "`1234567890-=\n"
-    txt += "~!@#$%^&*()_+\n"
-    txt += "qwertyuiop[]\\\n"
-    txt += "QWERTYUIOP{}|\n"
-    txt += "asdfghjkl;'\n"
-    txt += 'ASDFGHJKL:"\n'
-    txt += "zxcvbnm,./\n"
-    txt += "ZXCVBNM<>?\n"
+    txt += "`1234567890-="
+    txt += "~!@#$%^&*()_+"
+    txt += "qwertyuiop[]\\"
+    txt += "QWERTYUIOP{}|"
+    txt += "asdfghjkl;'"
+    txt += 'ASDFGHJKL:"'
+    txt += "zxcvbnm,./"
+    txt += "ZXCVBNM<>?"
+    txt += " "
+    txt += "\n"
 
     print(txt, file=fp)
     print(txt)
