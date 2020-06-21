e8a549fe42776f731bccefc2e5875b38d140f109
qianqyu <qianqyu@tencent.com>
2020-06-21 15:01:13 +0800

    update gen template

--- template/gen.py
+++ template/gen.py
@@ -4,7 +4,14 @@ with open("template.txt", "w") as fp:
     txt += 16*chr(9608) + '\n'
     for i in range(7):
         txt += chr(9608) + '\n'
-    txt += "abcdefghigklmnopqrstuvwxyz\n"
-    txt += "ABCDEFGHIGKLMNOPQRSTUVWXYZ\n"
+    txt += "`1234567890-=\n"
+    txt += "~!@#$%^&*()_+\n"
+    txt += "qwertyuiop[]\\\n"
+    txt += "QWERTYUIOP{}|\n"
+    txt += "asdfghjkl;'\n"
+    txt += 'ASDFGHJKL:"\n'
+    txt += "zxcvbnm,./\n"
+    txt += "ZXCVBNM<>?\n"
+
     print(txt, file=fp)
     print(txt)
