292b741440d24b8ac7a9695a790f3cc271181587
qianqyu <qianqyu@tencent.com>
2020-06-22 16:52:24 +0800

    update do.sh

--- do.sh
+++ do.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 mkdir -p output
-for ifile in ./pics/*.png
+rm -rf output/$1.v
+touch output/$1.v
+#for ifile in pics/$1/*.png
+#do
+#    ofile_ext=$(basename "$ifile")
+#    ofile=./pics/$1/${ofile_ext%.*}.v
+#   ./gen_txt.py -i $ifile -o $ofile -t templ_black.png -c Black 
+#    echo  $ofile Done!
+#done
+for ifile in pics/$1/*.v
 do
-    ofile_ext=$(basename "$ifile")
-    ofile=./output/${ofile_ext%.*}.v
-    ./gen_txt.py -i $ifile -o test_1.v -t templ_black.png -c Black 
-    echo  $ofile Done!
+    cat $ifile >> ./output/$1.v
 done
